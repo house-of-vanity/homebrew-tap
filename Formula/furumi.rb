4dea4cb9ae627c969ebcb5b5815f99239a1208d5
Ultradesu <ultradesu@hexor.cy>
2026-07-27 17:43:52 +0100

    Bump furumi_tui

--- Formula/furumi.rb
+++ Formula/furumi.rb
@@ -2,13 +2,13 @@ class Furumi < Formula
   desc "Federated P2P player for personal music libraries"
   homepage "https://github.com/house-of-vanity/furumi_tui"
   version "0.1.8"
+  sha256 "f8be8cd1aea57ac6e4043397d6cff1a9030df10af7d84f9570d6763b244428f5"
   license "WTFPL"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/house-of-vanity/furumi_tui/releases/download/latest/furumi-macos-aarch64-0.1.8.tar.gz"
-      # Replace after publishing the corrected v0.1.6 release asset.
-      sha256 "f8be8cd1aea57ac6e4043397d6cff1a9030df10af7d84f9570d6763b244428f5"
+      url "https://github.com/house-of-vanity/furumi_tui/releases/download/v#{version}/furumi-macos-aarch64-#{version}.tar.gz"
+      sha256 "#{sha256}"
     else
       odie "Intel macOS is not supported. Only ARM64 (Apple Silicon) is available."
     end
